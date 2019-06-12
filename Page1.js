cc81d744e54c5a0dcf2033fbb4f495b5c491d4e3
deadpool <hanikhellafi@gmail.com>
2019-06-12 15:17:20 -0500

    writing hotel function

--- Page1.js
+++ Page1.js
@@ -60,5 +60,45 @@ $(document).ready(function () {
 
 
 $("#submit-b").on("click",function(){
-  
+  hotel();
 })
+
+
+function hotel(){
+
+  $.ajax({
+    url: 'https://test.api.amadeus.com/v1/security/oauth2/token',//Access URL goes here
+    method: 'POST',
+    dataType: 'text',
+    data: {
+        scope:"read",
+        client_id: "mX4zCh7q8HuQGL0Nns3492LxojXRNMWL",//client id
+        client_secret: "nJCI4E4HNACd0yvL",//client secret id
+        grant_type: 'client_credentials'
+    },
+    headers: {
+        'Accept': 'application/json, application/x-www-form-urlencoded',
+        'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    complete: function (data) {
+         
+        /* YOUR WORK STARTS HERE! */
+        console.log(data);
+        parsed= JSON.parse(data.responseText);
+        token=parsed.access_token;
+        console.log(token);
+        $.ajax({
+          url: "https://test.api.amadeus.com/v2/shopping/hotel-offers?cityCode=CHI",
+          method: "GET",
+          headers: {
+              "Authorization" : "Bearer " + token
+          },
+          success: function(response) {
+             
+              console.log(response);
+          }
+      });
+    }
+});
+
+}
