52bcc65f88d217dcf8678f1b91c54ae4fc73ef36
Matthew Heimark <matthew.heimark@gmail.com>
2019-06-13 20:47:32 -0500

    added uber.js

--- assets/javascript/Page1.js
+++ assets/javascript/Page1.js
@@ -1,12 +1,12 @@
+
+//  document.addEventListener('DOMContentLoaded', function() {
+//   const elems = document.querySelectorAll('.sidenav');
+//    M.Sidenav.init(elems, options);
+// });
  
- document.addEventListener('DOMContentLoaded', function() {
-  const elems = document.querySelectorAll('.sidenav');
-   M.Sidenav.init(elems, options);
-});
- 
- $(document).ready(function(){
-  $('.sidenav').sidenav();
-});-
+//  $(document).ready(function(){
+//   $('.sidenav').sidenav();
+// });
 
 //document.addEventListener('DOMContentLoaded', function () {
   //var elems = document.querySelectorAll('.slider');
@@ -41,28 +41,68 @@ $(document).ready(function () {
       "Dallas-Ft.Worth (DFW)": null,
       "Key West (EYW)": null,
       "New Orleans(MSY)": null,
+
+
     },
   });
 });
 
-//Scrollspy function
-document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.scrollspy');
-  var instances = M.ScrollSpy.init(elems, options);
+// //Scrollspy function
+// document.addEventListener('DOMContentLoaded', function () {
+//   var elems = document.querySelectorAll('.scrollspy');
+//   var instances = M.ScrollSpy.init(elems, options);
+// });
+
+
+$(document).ready(function () {
+  $('.pushpin').pushpin();
 });
 
 
+$("#submit-b").on("click",function(){
+  hotel();
+})
 
 
+function hotel(){
 
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.parallax');
-   M.Parallax.init(elems, options);
+  $.ajax({
+    url: 'https://test.api.amadeus.com/v1/security/oauth2/token',//Access URL goes here
+    method: 'POST',
+    dataType: 'text',
+    data: {
+        scope:"read",
+        client_id: "mX4zCh7q8HuQGL0Nns3492LxojXRNMWL",//client id
+        client_secret: "nJCI4E4HNACd0yvL",//client secret id
+        grant_type: 'client_credentials'
+    },
+    headers: {
+        'Accept': 'application/json, application/x-www-form-urlencoded',
+        'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    complete: function (data) {
+         
+        /* YOUR WORK STARTS HERE! */
+        console.log(data);
+        parsed= JSON.parse(data.responseText);
+        token=parsed.access_token;
+        console.log(token);
+        $.ajax({
+          url: "https://test.api.amadeus.com/v2/shopping/hotel-offers?cityCode=CHI&adults=1&radius=10&radiusUnit=KM&paymentPolicy=NONE&includeClosed=false&bestRateOnly=false&view=FULL",
+          method: "GET",
+          headers: {
+              "Authorization" : "Bearer " + token
+          },
+          success: function(response) {
+             
+              console.log(response);
+              localStorage.clear();
+              hotelist=JSON.stringify(response);
+              localStorage.setItem("hotel-list", hotelist);
+            
+          }
+      });
+    }
 });
 
-// Or with jQuery
-
-$(document).ready(function(){
-  $('.parallax').parallax();
-});
-      
+}
