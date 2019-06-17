795d3032aa0414cc326a45783b7517c78d010fa6
deadpool <hanikhellafi@gmail.com>
2019-06-17 14:51:12 -0500

    function to get current location + showing current location on the map

--- assets/javascript/page2.js
+++ assets/javascript/page2.js
@@ -11,6 +11,7 @@
 // susan
 var map;
 var hotelData;
+var myposition;
 
 function initMap() {
     hotel();
@@ -38,15 +39,6 @@ function listHotel() {
     for (i = 0; i < hotelData.length; i++) {
 
 
-<<<<<<< HEAD
-      var card = $("<div class='card small' id='hotel-card'  >" +
-        "<div class='card-image waves-effect waves-block waves-light'>" +
-        "<img class='activator hotel1' src='./assets/image/hotel.jpg'/>" +
-        "</div>" +
-        "<div class='card-content'>" +
-        "<span class='card-title activator grey-text text-darken-4 hotelName1' id='hotel" + i + "'> <h5" +
-        " class=' right green-text' id='price"+i+"'>$$</h5></span>" +
-=======
         var card = $("<div class='card small' id='hotel-card'  >" +
             "<div class='card-image waves-effect waves-block waves-light'>" +
             "<img class='activator hotel1' src='./assets/hotel-images/hotel" + i + ".jpg'/>" +
@@ -77,27 +69,9 @@ function listHotel() {
             "</div>" +
             "</div>");
 
->>>>>>> e22c168691e38cbadd04a2fceb028994ec9ab6e3
 
         $("#append").append(card);
 
-<<<<<<< HEAD
-        "</div>" +
-        "<div class='card-reveal'>" +
-        "<span class='card-title grey-text text-darken-4' id='hotel-r" + i + "'><i" +
-        " class='material-icons right'>close</i></span>" +
-        "<div class='hotelFeatures1 grey-text'>" +
-        "<h5>Features: </h5>"+
-        
-        "<h6 class='black-text' id='description"+i+"'> Rating: </h6>"+
-        "<h6 class='black-text' id='address"+i+"'> Address : </h6>"+
-        "<h6 class='green-text' id='amenities"+i+"'> Amenities : </h6>"+
-        "<h6 class='black-text' id='contact"+i+"'> Contact : </h6>"+
-        
-
-        "</div>" +
-=======
->>>>>>> e22c168691e38cbadd04a2fceb028994ec9ab6e3
 
         //     // card.html(hotelCard);
         //     // card.attr("class","card");
@@ -109,40 +83,26 @@ function listHotel() {
 
         $("#address" + i).append(hotelData[i].hotel.address.lines[0]);
         $("#address" + i).append(", " + hotelData[i].hotel.address.cityName);
-        if (hotelData[i].hotel.amenities !== "undefined") {
+        try {
             for (j = 0; j < hotelData[i].hotel.amenities.length; j++) {
                 $("#amenities" + i).append(hotelData[i].hotel.amenities[j].toLowerCase().replace(/_/g, " ") + ", ");
             }
         }
+        catch(e){
+            $("#amenities" + i).append("not available");
+        }
 
-        console.log(hotelData[i].hotel.contact.phone);
+        
 
-<<<<<<< HEAD
-      //     // card.html(hotelCard);
-      //     // card.attr("class","card");
-      //     // $(".hotelName1").attr("id","hotel"+i);
-      $("#hotel" + i).append(hotelData[i].hotel.name);
-      $("#hotel-r" + i).append(hotelData[i].hotel.name);
-    
-      $("#description" + i).append(hotelData[i].hotel.rating);  
-      
-      $("#address" + i).append(hotelData[i].hotel.address.lines[0]); 
-      $("#address" + i).append(", "+hotelData[i].hotel.address.cityName); 
-      for(j=0;j<hotelData[i].hotel.amenities.length;j++){
-      $("#amenities" + i).append(hotelData[i].hotel.amenities[j].toLowerCase().replace(/_/g," ")+", "); 
-      }
-      $("#contact" + i).append(hotelData[i].hotel.contact.phone);
-      $("#price" + i).html(hotelData[i].offers[0].price.total+"$"); 
-      console.log(hotelData[i].offers[0].price.total);
-=======
-
-
-        $("#contact" + i).append(hotelData[i].hotel.contact.phone);
+         try{
+        $("#contact" + i).append(hotelData[i].hotel.contact.phone);}
+        catch(e){
+            $("#contact" + i).append("not available")
+        }
 
         $("#price" + i).html(hotelData[i].offers[0].price.total + "$");
 
         console.log(hotelData[i].offers[0].price.total);
->>>>>>> e22c168691e38cbadd04a2fceb028994ec9ab6e3
 
     }
 
@@ -228,34 +188,72 @@ function hotel() {
                     console.log(hotelData);
 
 
+                    x= navigator.geolocation;
+                    x.getCurrentPosition(success);
+                    function success(data){
+                        myposition=data.coords
+                        map = new google.maps.Map(document.getElementById('map'), {
+                            center: { lat: hotelData[0].hotel.latitude, lng: hotelData[0].hotel.longitude },
+                            zoom: 11
+                        });
+                        
+                        console.log(myposition.longitude);
+                        localStorage.setItem("my-lat",myposition.latitude);
+                        localStorage.setItem("my-long",myposition.longitude);
+                        mypos = new google.maps.Marker({
+                            position: {
+                                lat:myposition.latitude,
+                                lng:myposition.longitude,
+                                
+                            },
+    
+                            map:map,
+                            
+    
+                            title: "my position",
+                            
+                            
+                            
+                        });
+                        mypos.setMap(map);
+                        var marker = [];
 
-                    map = new google.maps.Map(document.getElementById('map'), {
-                        center: { lat: hotelData[0].hotel.latitude, lng: hotelData[0].hotel.longitude },
-                        zoom: 13
-                    });
+                        for (i = 0; i < hotelData.length; i++) {
+                            pin = new google.maps.Marker({
+                                    position: {
+                                        lat: hotelData[i].hotel.latitude,
+                                        lng: hotelData[i].hotel.longitude
+                                    },
+    
+                                    title: hotelData[i].hotel.name,
+                                    icon: {url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"}
+                                })
+                                marker.push(pin);
+                        }
+                        console.log(marker);
+    
+                        for (i = 0; i < marker.length; i++) {
+                            marker[i].setMap(map);
+    
+                        }
+                        gethotel();
 
-
-                    var marker = []
-
-                    for (i = 0; i < hotelData.length; i++) {
-                        pin = new google.maps.Marker({
-                                position: {
-                                    lat: hotelData[i].hotel.latitude,
-                                    lng: hotelData[i].hotel.longitude
-                                },
-
-                                title: hotelData[i].hotel.name,
-                            }),
-                            marker.push(pin);
                     }
-                    console.log(marker);
 
-                    for (i = 0; i < marker.length; i++) {
-                        marker[i].setMap(map);
+                   
 
-                    }
 
-                    gethotel();
+                   
+
+                   
+                    
+                    
+
+
+                   
+                    
+
+                   
 
                 }
             });
